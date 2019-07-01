e28e08a46ef6ace6fd83b19011a7d2bac7999190
Tyler-Pritchard <pritchard.tyler@gmail.com>
2019-06-30 20:07:14 -0700

    add error helper function to LoginForm

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -18,6 +18,18 @@ class LoginForm extends Component {
         this.props.loginUser({ email, password })
     }
 
+    renderError() {
+        if (this.props.error) {
+            return (
+                <View style={{ backgroundColor: 'white' }}>
+                    <Text style={styles.errorTextStyle}>
+                        {this.props.error}
+                    </Text>
+                </View>
+            );
+        }
+    }
+
     render() {
         return (
             <Card>
@@ -53,7 +65,8 @@ class LoginForm extends Component {
 const mapStateToProps = state => {
     return {
         email: state.auth.email,
-        password: state.auth.password
+        password: state.auth.password,
+        error: state.auth.error
     }
 }
 
