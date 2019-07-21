fb76b53c81dfb3565c2751390bd2951cbaf1903c
Tyler-Pritchard <pritchard.tyler@gmail.com>
2019-07-20 21:34:05 -0700

    Employee edit

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -1,10 +1,38 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import EmployeeForm from  './EmployeeForm';
+import  { employeeUpdate } from '../actions';
 import { Card,  CardSection, Button } from './common';
 
 class EmployeeEdit extends Component {
-    render() {
+    componentWillMount() {
+        _.each(this.props.employee, (value, prop) => {
+            this.props.employeeUpdate({ prop, value });
+        });
+    }
 
+    onButtonPress() {
+        const { name, phone,shift } = this.props;
+        console.log(name, phone, shift);
+    }
+
+    render() {
+        <Card>
+            <EmployeeForm />
+            <CardSection>
+                <Button onPress={this.onButtonPress.bind(this)}>
+                    Save Changes
+                </Button>
+            </CardSection>
+        </Card>
     }
 }
 
-export default EmployeeEdit;
+const mapStateToProps = (state => {
+    const { name, phone,shift } = state.employeeForm;
+
+    return { name, phone,shift };
+})
+
+export default connect(mapStateToProps, { employeeUpdate })(EmployeeEdit);
