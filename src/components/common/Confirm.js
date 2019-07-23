d37e004c14395228dba983a97a4f53b3caed548c
Tyler-Pritchard <pritchard.tyler@gmail.com>
2019-07-23 14:23:51 -0700

    add modal state

--- src/components/common/Confirm.js
+++ src/components/common/Confirm.js
@@ -4,15 +4,17 @@ import { CardSection } from './CardSection';
 import { Button } from './Button';
 
 const Confirm = ({ children, visible, onAccept,onDecline }) => {
+
+    return (
     <Modal
         visible={visible}
         transparent
         animationType="slide"
         onRequestClose={() => {}}
     >
-        <View>
-            <CardSection>
-                <Text>{children}</Text>
+        <View style={containerStyle}>
+            <CardSection style={cardSectionStyle}>
+                <Text style={textStyle}>{children}</Text>
             </CardSection>
 
             <CardSection>
@@ -21,6 +23,26 @@ const Confirm = ({ children, visible, onAccept,onDecline }) => {
             </CardSection>
         </View>
     </Modal>
+    );
 };
+
+const  styles = {
+    condSectionStyle: {
+        justifyContent: 'center'
+    },
+    textStyle: {
+        flex: 1,
+        fontSize: 18,
+        textAlign: 'center',
+        lineHeight: 40
+    },
+    containerStyle: {
+        backgroundColor: 'rgba(0, 0, 0, 0.75)',
+        position: 'relative',
+        flex: 1,
+        justifyContent: 'center'
+
+    }
+}
 
 export { Confirm };
